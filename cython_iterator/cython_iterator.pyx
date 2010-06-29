3149dcbcf83a2ef653f6e439b6f8dd9b79acf932
Aaron DeVore <aaron.devore@gmail.com>
2010-06-28 18:42:36 -0700

    Oops, typo

--- cython_iterator/cython_iterator.pyx
+++ cython_iterator/cython_iterator.pyx
@@ -1,14 +1,22 @@
-
-# collatz iterator implementation
+# Use a very fast iterator that calculates values in C instead of pre
+# calculating and storing in memory. Matplotlib can directly use this
+# iterator when combined with xrange(high)
+# 
+# plot(xrange(2, high + 1), CollatzGenerator(high))
+# 
+# Requires: Cython, python headers (sometimes in python-dev package)
+# Cython does not currently support generators, so manually write the
+# classes
 
 cdef class CollatzGenerator:
     cdef unsigned int high
-    def __cinit__(self unsigned int high):
+    def __cinit__(self, unsigned int high):
         assert high > 2
         self.high = high
 
     def __iter__(self):
         return CollatzIterator(self.high)
+
 
 cdef class CollatzIterator:
     cdef unsigned int i, high
@@ -23,7 +31,7 @@ cdef class CollatzIterator:
 
     def __next__(self):
         cdef unsigned int n, length
-        if self.i < self.high:
+        if self.i > self.high:
             raise StopIteration
         n = self.i
         length = 0
@@ -35,3 +43,14 @@ cdef class CollatzIterator:
             length += 1
         self.i += 1
         return length
+
+cdef class CollatzEnumIterator(CollatzIterator):
+    def __next__(self):
+        cdef unsigned int length, i
+        i = self.i # keep i before running __next__
+        length = CollatzIterator.__next__(self)
+        return (i, length)
+
+cdef class CollatzEnumGenerator(CollatzGenerator):
+    def __iter__(self):
+        return CollatzEnumIterator(self.high)
